dc2eff7dfa2fae7b31a508c51cb90cf8fe1f0939
senatrius <g.makutenas@gmail.com>
2023-02-22 23:47:57 +0200

    add date to news bar

--- src/components/NewsBar.tsx
+++ src/components/NewsBar.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
+const date = new Date();
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+const getOrdinalNum = (number: number) => {
+  let selector;
+
+  if (number <= 0) {
+    selector = 4;
+  } else if ((number > 3 && number < 21) || number % 10 > 3) {
+    selector = 0;
+  } else {
+    selector = number % 10;
+  }
+
+  return number + ['th', 'st', 'nd', 'rd', ''][selector];
+};
+
 export const NewsBar = () => {
   return (
     <div className='bg-navBg py-3 text-small text-white'>
@@ -7,7 +37,9 @@ export const NewsBar = () => {
         <p>Latest news placeholder here...</p>
         <div className='flex items-center'>
           <div className='mx-4 -my-3 hidden min-h-full w-[1px] self-stretch bg-divider sm:inline-block'></div>
-          <p className='hidden sm:inline-block '>20XX, Janembruary 99th</p>
+          <p className='hidden sm:inline-block '>{`${date.getFullYear()}, ${
+            months[date.getMonth()]
+          } ${getOrdinalNum(date.getDate())}`}</p>
           <div className='mx-4 -my-3 min-h-full w-[1px] self-stretch bg-divider'></div>
           <p>- XX°C</p>
         </div>
