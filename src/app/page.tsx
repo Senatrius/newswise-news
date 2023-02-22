56643d137bf8cf844e1f3bcd4ae0dd715261bb77
senatrius <g.makutenas@gmail.com>
2023-02-23 01:18:25 +0200

    add social section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { CalendarSection } from '@/components/Calendar';
+import { Social } from '@/components/Social';
 import { TopStories } from '@/components/TopStories';
 
 export default function Home() {
@@ -6,11 +7,11 @@ export default function Home() {
     <main>
       <TopStories />
       <div className='wrapper my-8 grid grid-cols-12 gap-4 md:gap-6'>
-        <section className='col-span-12 md:col-span-4 lg:col-span-3'>
+        <section className='col-span-12 flex flex-col gap-4 md:col-span-4 md:gap-6'>
           <CalendarSection />
+          <Social />
         </section>
-        <section className='col-span-7 md:col-span-8 lg:col-span-6'></section>
-        <section className='hidden lg:col-span-3'></section>
+        <section className='col-span-7 md:col-span-8'></section>
       </div>
     </main>
   );
