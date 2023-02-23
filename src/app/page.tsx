ccb1cc84e8c787d809c115e131c144beec9a6d85
senatrius <g.makutenas@gmail.com>
2023-02-23 13:31:10 +0200

    add category placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { CalendarSection } from '@/components/Calendar';
+import { Category } from '@/components/Category';
 import { TopStories } from '@/components/TopStories';
 
 export default function Home() {
@@ -6,7 +7,15 @@ export default function Home() {
     <main>
       <TopStories />
       <div className='wrapper my-8 grid grid-cols-12 gap-4 md:gap-6'>
-        <section className='col-span-7 md:col-span-8'></section>
+        <section className='col-span-12 flex flex-col gap-8 md:col-span-8'>
+          <Category category='Business' />
+          <Category category='Entertainment' />
+          <Category category='Sport' />
+          <Category category='Politics' />
+          <Category category='Technology' />
+          <Category category='Science' />
+          <Category category='Health' />
+        </section>
         <section className='col-span-12 md:col-span-4'>
           <CalendarSection />
         </section>
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -5,6 +5,50 @@ export const Category = ({ category }: { category: string }) => {
   return (
     <section>
       <SectionHeading heading={category} />
+      <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2'>
+        <div className='col-span-1'>
+          <picture className='mb-4 block aspect-[3/2] max-h-[12rem] w-full bg-navBg'>
+            a
+          </picture>
+          <h3 className='mb-1.5 text-card font-bold'>Placeholder title</h3>
+          <p className='line-clamp-3'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            non error sed est animi assumenda? Placeholder description.
+          </p>
+        </div>
+        <div className='col-span-1 flex flex-col gap-4'>
+          <div className='flex gap-4'>
+            <picture className='block aspect-square w-full max-w-[5rem] grow bg-navBg'></picture>
+            <div className='grow'>
+              <h3 className='mb-1.5 text-card font-bold'>Placeholder title</h3>
+              <p className='line-clamp-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+                architecto? Placeholder description.
+              </p>
+            </div>
+          </div>
+          <div className='flex gap-4'>
+            <picture className='block aspect-square w-full max-w-[5rem] grow bg-navBg'></picture>
+            <div className='grow'>
+              <h3 className='mb-1.5 text-card font-bold'>Placeholder title</h3>
+              <p className='line-clamp-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+                architecto? Placeholder description.
+              </p>
+            </div>
+          </div>
+          <div className='flex gap-4'>
+            <picture className='block aspect-square w-full max-w-[5rem] grow bg-navBg'></picture>
+            <div className='grow'>
+              <h3 className='mb-1.5 text-card font-bold'>Placeholder title</h3>
+              <p className='line-clamp-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+                architecto? Placeholder description.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
